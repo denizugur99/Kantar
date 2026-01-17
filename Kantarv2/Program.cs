135057194a367a9d6d12060039083c48228e7b53
denizugur99 <denougur0@gmail.com>
2026-01-17 16:44:23 +0300

    docker

--- Kantarv2/Program.cs
+++ Kantarv2/Program.cs
@@ -184,11 +184,8 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.MapScalarApiReference();
-}
+app.MapOpenApi();
+app.MapScalarApiReference();
 
 app.UseHttpsRedirection();
 
